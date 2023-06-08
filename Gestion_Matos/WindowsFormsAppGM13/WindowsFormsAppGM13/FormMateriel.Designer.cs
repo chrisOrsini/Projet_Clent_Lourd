0c03f559fbcf97a3d11319e94935df73b479501e
chrisOrsini <chriskoutsotsa@gmail.com>
2023-06-08 15:37:16 +0200

    Retrouver mon projetr Gestion_Matos

---
+++ Gestion_Matos/WindowsFormsAppGM13/WindowsFormsAppGM13/FormMateriel.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace WindowsFormsAppGM13
+{
+    partial class FormMateriel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxMatériel = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxMTBF = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxService = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonFermer = new System.Windows.Forms.Button();
+            this.buttonSupprimer = new System.Windows.Forms.Button();
+            this.buttonModifier = new System.Windows.Forms.Button();
+            this.buttonAjouter = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.textBoxMarque = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBoxClient = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matériel";
+            // 
+            // listBoxMatériel
+            // 
+            this.listBoxMatériel.FormattingEnabled = true;
+            this.listBoxMatériel.ItemHeight = 16;
+            this.listBoxMatériel.Location = new System.Drawing.Point(21, 54);
+            this.listBoxMatériel.Name = "listBoxMatériel";
+            this.listBoxMatériel.Size = new System.Drawing.Size(205, 356);
+            this.listBoxMatériel.TabIndex = 1;
+            this.listBoxMatériel.SelectedIndexChanged += new System.EventHandler(this.listBoxMatériel_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxClient);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBoxMarque);
+            this.groupBox1.Controls.Add(this.textBoxType);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxMTBF);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxDate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxService);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxNom);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(271, 54);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(311, 367);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informations";
+            // 
+            // textBoxMTBF
+            // 
+            this.textBoxMTBF.Location = new System.Drawing.Point(103, 174);
+            this.textBoxMTBF.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxMTBF.Name = "textBoxMTBF";
+            this.textBoxMTBF.Size = new System.Drawing.Size(181, 22);
+            this.textBoxMTBF.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 180);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "MTBF";
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Location = new System.Drawing.Point(103, 122);
+            this.textBoxDate.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(181, 22);
+            this.textBoxDate.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 125);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Date_Install";
+            // 
+            // textBoxService
+            // 
+            this.textBoxService.Location = new System.Drawing.Point(103, 73);
+            this.textBoxService.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxService.Name = "textBoxService";
+            this.textBoxService.Size = new System.Drawing.Size(181, 22);
+            this.textBoxService.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 79);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "NoService";
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(103, 26);
+            this.textBoxNom.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(181, 22);
+            this.textBoxNom.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom";
+            // 
+            // buttonFermer
+            // 
+            this.buttonFermer.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonFermer.Location = new System.Drawing.Point(639, 289);
+            this.buttonFermer.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonFermer.Name = "buttonFermer";
+            this.buttonFermer.Size = new System.Drawing.Size(135, 42);
+            this.buttonFermer.TabIndex = 10;
+            this.buttonFermer.Text = "Fermer";
+            this.buttonFermer.UseVisualStyleBackColor = true;
+            // 
+            // buttonSupprimer
+            // 
+            this.buttonSupprimer.Location = new System.Drawing.Point(639, 208);
+            this.buttonSupprimer.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSupprimer.Name = "buttonSupprimer";
+            this.buttonSupprimer.Size = new System.Drawing.Size(135, 42);
+            this.buttonSupprimer.TabIndex = 9;
+            this.buttonSupprimer.Text = "Supprimer";
+            this.buttonSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // buttonModifier
+            // 
+            this.buttonModifier.Location = new System.Drawing.Point(639, 129);
+            this.buttonModifier.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonModifier.Name = "buttonModifier";
+            this.buttonModifier.Size = new System.Drawing.Size(135, 42);
+            this.buttonModifier.TabIndex = 8;
+            this.buttonModifier.Text = "Modifier";
+            this.buttonModifier.UseVisualStyleBackColor = true;
+            // 
+            // buttonAjouter
+            // 
+            this.buttonAjouter.Location = new System.Drawing.Point(639, 54);
+            this.buttonAjouter.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonAjouter.Name = "buttonAjouter";
+            this.buttonAjouter.Size = new System.Drawing.Size(135, 42);
+            this.buttonAjouter.TabIndex = 7;
+            this.buttonAjouter.Text = "Ajouter";
+            this.buttonAjouter.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 274);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Marque";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 218);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Type";
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Location = new System.Drawing.Point(103, 215);
+            this.textBoxType.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.Size = new System.Drawing.Size(181, 22);
+            this.textBoxType.TabIndex = 11;
+            // 
+            // textBoxMarque
+            // 
+            this.textBoxMarque.Location = new System.Drawing.Point(103, 268);
+            this.textBoxMarque.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxMarque.Name = "textBoxMarque";
+            this.textBoxMarque.Size = new System.Drawing.Size(181, 22);
+            this.textBoxMarque.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 321);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Client";
+            // 
+            // comboBoxClient
+            // 
+            this.comboBoxClient.FormattingEnabled = true;
+            this.comboBoxClient.Location = new System.Drawing.Point(103, 318);
+            this.comboBoxClient.Name = "comboBoxClient";
+            this.comboBoxClient.Size = new System.Drawing.Size(181, 24);
+            this.comboBoxClient.TabIndex = 14;
+            // 
+            // FormMateriel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonFermer);
+            this.Controls.Add(this.buttonSupprimer);
+            this.Controls.Add(this.buttonModifier);
+            this.Controls.Add(this.buttonAjouter);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBoxMatériel);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMateriel";
+            this.Text = "Gestion Matériel";
+            this.Load += new System.EventHandler(this.FormMateriel_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxMatériel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxMTBF;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxService;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonFermer;
+        private System.Windows.Forms.Button buttonSupprimer;
+        private System.Windows.Forms.Button buttonModifier;
+        private System.Windows.Forms.Button buttonAjouter;
+        private System.Windows.Forms.TextBox textBoxMarque;
+        private System.Windows.Forms.TextBox textBoxType;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBoxClient;
+    }
+}
---
+++ Gestion_Matos/WindowsFormsAppGM13/WindowsFormsAppGM13/FormMateriel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppGM13
+{
+    public partial class FormMateriel : Form
+    {
+
+        private string connstring = @"Server=.\SQLEXPRESS;Database=GM;Trusted_Connection=True;";
+
+        public FormMateriel()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMateriel_Load(object sender, EventArgs e)
+        {
+            affcMatos();
+        }
+
+        private void affcMatos()
+        {
+            listBoxMatériel.Items.Clear();
+
+            SqlConnection cn = null;
+            SqlCommand com = null;
+            SqlDataReader sqr = null;
+
+            try
+            {
+                cn = new SqlConnection(this.connstring);
+                cn.Open();
+
+                string strsql = "select Nom from Materiel";
+                com = new SqlCommand(strsql, cn);
+                sqr = com.ExecuteReader();
+
+                string str;
+                while (sqr.Read() == true)
+                {
+                    str = sqr["Nom"].ToString();
+                    listBoxMatériel.Items.Add(str);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problème avec votre base de données, contactez votre admin !");
+                Application.Exit();
+            }
+
+            if (cn != null)
+            {
+                cn.Close();
+                cn.Dispose();
+                if (com != null)
+                {
+                    com.Dispose();
+                    if (sqr != null)
+                    {
+                        sqr.Close();
+                    }
+                }
+            }
+        }
+
+        
+
+        private void listBoxMatériel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxMatériel.SelectedItem == null)
+            {
+                return;
+            }
+
+            string nom = listBoxMatériel.SelectedItem.ToString();
+
+            SqlConnection cn = null;
+            SqlCommand com = null;
+            SqlDataReader sqr = null;
+
+            cn = new SqlConnection(this.connstring);
+            cn.Open();
+
+            string strsql = "select * from Materiel where Nom = '" + nom + "'";
+            com = new SqlCommand(strsql, cn);
+            sqr = com.ExecuteReader();
+            sqr.Read();
+
+            textBoxNom.Text = sqr["Nom"].ToString();
+            textBoxService.Text = sqr["NoSerie"].ToString();
+            textBoxDate.Text = sqr["Date_Install"].ToString();
+            textBoxMTBF.Text = sqr["MTBF"].ToString();
+            textBoxType.Text = sqr["Type"].ToString();
+            textBoxMarque.Text = sqr["Marque"].ToString();
+
+            //fixer le Client
+
+
+            if (cn != null)
+            {
+                cn.Close();
+                cn.Dispose();
+                if (com != null)
+                {
+                    com.Dispose();
+                    if (sqr != null)
+                    {
+                        sqr.Close();
+                    }
+                }
+            }
+        }
+
+
+    }
+}
